75e4eecf7fc0d836e05a266ff232e8f261c22407
Emanuele <55718523+JutJ3di@users.noreply.github.com>
2021-01-13 10:29:11 +0100

    Create header.h

---
+++ prod_cons_queue_async/header.h
@@ -0,0 +1,16 @@
+				/*-----HEADER FILE----*/
+#ifndef __HEADER
+#define __HEADER
+#define MESSAGGIO 1
+
+typedef char msg [40];
+
+typedef struct {
+		long tipo;
+		msg mess;
+} Messaggio;
+
+void Produttore(int queue,char* m);
+void Consumatore(int queue);
+void printMsgInfo(int queue);
+#endif
